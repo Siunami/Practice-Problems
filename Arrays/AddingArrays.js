6eca283ff007c14d373651e534a40abbcca72366
Matthew <siunami.matt@gmail.com>
2015-12-04 13:11:14 -0800

    New project, add array values together

--- Arrays/AddingArrays.js
+++ Arrays/AddingArrays.js
@@ -1,9 +1,10 @@
-console.log(range(1,6));
+/*Insert a range of increasing numbers to add together*/
+console.log(range(-9, 10));
+/*returns 10*/
 
 function range(a, b, step){
   if (step == null) step = 1;
   var list = [];
-
   if (step > 0){
     for(var i = a; i <= b; i += step)
       list.push(i);
@@ -11,5 +12,8 @@ function range(a, b, step){
     for (var i = a; i >= b; i += step)
       list.push(i);
   }
-  return list;
+  var sum = list.reduce(function(a, b) {
+    return a + b;
+  });
+  return sum;
 }
